265b21d10d517d30048a31f6aef5ccb62a3bb7c4
r1beirin <mateus.vaz@ufu.br>
2022-06-09 14:15:54 -0300

    Finalizado roteiro 1

--- roteiros/roteiro1.pl
+++ roteiros/roteiro1.pl
@@ -38,40 +38,38 @@ mae(selma, ling).
 avoM(X, Y) :-
     pai(X, Z),
     pai(Z, Y);
+    pai(X, Z),
     mae(Z, Y).
 
 avoF(X, Y) :-
     mae(X, Z),
     pai(Z, Y);
+    mae(X, Z),
     mae(Z, Y).
 
 filho(X, Y) :-
     homem(X),
     pai(Y, X);
+    homem(X),
     mae(Y, X).
 
 filha(X, Y) :-
     mulher(X),
     pai(Y, X);
+    mulher(X),
     mae(Y, X).
 
 irmao(X, Y) :-
     homem(X),
     not(X = Y),
     pai(Pai, X),
-    pai(Pai, Y);
-    not(X = Y),
-    mae(Mae, X),
-    mae(Mae, Y).
+    pai(Pai, Y).
 
 irma(X, Y) :-
     mulher(X),
     not(X = Y),
     pai(Pai, X),
-    pai(Pai, Y);
-    not(X = Y),
-    mae(Mae, X),
-    mae(Mae, Y).
+    pai(Pai, Y).
 
 irmaos(X, Y) :- 
     not(X = Y),
@@ -81,23 +79,26 @@ irmaos(X, Y) :-
 tio(X, Y) :-
     homem(X),
     pai(Pai, Y),
-    irmaos(X, Pai);
-    mae(Mae, Y),
-    homem(X),
-    irmaos(X, Mae).
-
-tia(X, Y) :-
-    mae(Mae, Y),
-    irmaos(X, Mae);
-    pai(Pai, Y),
-    mulher(X),
     irmaos(X, Pai).
 
-% terminar prima
+tia(X, Y) :-
+    mulher(X),
+    mae(Mae, Y),
+    irmaos(X, Mae);
+    mulher(X),
+    pai(Pai, Y),
+    irmaos(X, Pai).
+
 prima(X, Y) :-
+    mulher(X),
     mae(MaeX, X),
     mae(MaeY, Y),
-    mulher(X),
+    irma(MaeX, MaeY).
+
+primo(X, Y) :-
+    homem(X),
+    mae(MaeX, X),
+    mae(MaeY, Y),
     irma(MaeX, MaeY).
 
 /*
@@ -107,10 +108,22 @@ prima(X, Y) :-
 
     5. magico(elfo_domestico). -> Falso, pois não existe ninguém chamado elfo_domestico que está ligado aos predicados de elfo_domestico ou bruxo.
     6. feiticeiro(harry). -> Falso, pois não existe nenhum fato feiticeiro(harry) escrito.
-    7. magico(feiticeiro). -> Falso, pois o nome feiticeiro não é um elfo_domestico nem um bruxo.
+    7. magico(feiticeiro). -> Erro, pois o predicado feiticeiro não existe. Tratando esse erro, X unifica com feiticeiro mas não é um elfo_domestico nem um bruxo.
     8. magico('McGonagall'). -> True, pois existe um bruxo('McGonagall').
     9. magico(Hermione). -> O resultado da consulta são as unificações que a variavel Hermione pode fazer.
-    Fazer a arvore de busca de magico(Hermione). no papel.
+    
+    Consulta: magico(Hermione).
+
+    1ª - magico(Hermione) :- elfo_domestico(Hermione).
+    Hermione unifica com dobby.
+    2ª - magico(Hermione) :- feiticeiro(Hermione). 
+    Hermione não unifica com nada, então não é valido essa regra para variavel Hermione.
+    3ª - magico(Hermione) :- bruxo(Hermione).
+    Hermione unifica com hermione
+    Hermione unifica com 'McGonagall'
+    Hermione unifica com rita_skeeter
+
+    Fim.
 */
 :- dynamic feiticeiro/1.
 
@@ -128,12 +141,7 @@ magico(X) :- bruxo(X).
     Minigramática
 
     10. Consulta: sentenca(A,B,C,D,E).
-
-    A unifica com D
-
-    11 - Lista de sentenças:
-
-        uma pessoa come
+    11. ???
 */
 
 palavra(artigo, uma).
@@ -149,3 +157,36 @@ sentenca(Palavra1, Palavra2, Palavra3, Palavra4, Palavra5) :-
     palavra(verbo, Palavra3),
     palavra(artigo, Palavra4),
     palavra(nome, Palavra5).
+
+/*
+    Alunos
+    12. Ok
+    13. aluno(paulo, pl).
+    14. aluno(rui, poo).
+    15. aluno(paulo, ed1), aluno(maria, ed1).
+    16. aluno(X, pl).
+    17. aluno(rui, X).
+    18. feito no fazDisciplina/2.
+    19. A consulta quer unificar X com todos os alunos que estudam PL e estuda.
+    20. cursapl/1
+*/
+
+aluno(paulo,poo).
+aluno(pedro,poo).
+aluno(maria,pl).
+aluno(rui,pl).
+aluno(manuel,pl).
+aluno(pedro,pl).
+aluno(rui,ed1).
+
+estuda(paulo).
+estuda(maria).
+estuda(manuel).
+
+fazDisciplina(X, Y) :-
+    aluno(X, Y),
+    estuda(X).
+
+cursaPl(X) :-
+    aluno(X, pl),
+    estuda(X).
