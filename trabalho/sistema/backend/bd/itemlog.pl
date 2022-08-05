af414a09bd5de2ea4f7e6ec7143b402baad8dd6b
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-05 00:26:52 -0300

    Refacto to retract in itemLog atualiza

--- trabalho/sistema/backend/bd/itemlog.pl
+++ trabalho/sistema/backend/bd/itemlog.pl
@@ -32,6 +32,6 @@ atualiza(SeqLog, Ocorrencia, DataReg, UsuAdm, CodUsu) :-
     itemlog(SeqLog, _, _, _, _),
     integrante:integrante(CodUsu, _, _, _, _, _, _),
     with_mutex(itemlog,
-                (retractall_itemlog(SeqLog, _Ocorrencia, _DataReg, _UsuAdm, _CodUsu),
+                (retract_itemlog(SeqLog, _Ocorrencia, _DataReg, _UsuAdm, _CodUsu),
                 assert_itemlog(SeqLog, Ocorrencia, DataReg, UsuAdm, CodUsu))
                 ).
