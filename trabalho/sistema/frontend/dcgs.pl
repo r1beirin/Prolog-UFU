2f12057fe8ccbdf4f599a83e3bc4a46e8773498f
r1beirin <mateusriibeiro@gmail.com>
2022-08-18 09:55:29 -0300

    fix dcgs3

--- trabalho/sistema/frontend/dcgs.pl
+++ trabalho/sistema/frontend/dcgs.pl
@@ -1,3 +1,10 @@
+/* html//1, reply_html_page  */
+:- use_module(library(http/html_write)).
+/* html_requires  */
+:- use_module(library(http/html_head)).
+
+:- ensure_loaded(gabarito(boot5rest)).
+
 titulo_pagina(Title) -->
     html( div(class('text-center align-items-center w-100'),
               h1('display-3', Title))).
